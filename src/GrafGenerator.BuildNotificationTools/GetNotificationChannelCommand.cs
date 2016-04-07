cc14231ddc5480b25d0b776baf794e1afaee118d
nikita ivanov <ni.ivanov@2gis.ru>
2016-04-07 18:58:47 +0600

    add project for cmdlets

---
+++ src/GrafGenerator.BuildNotificationTools/GetNotificationChannelCommand.cs
@@ -0,0 +1,38 @@
+﻿using System.Management.Automation;
+
+namespace GrafGenerator.BuildNotificationTools
+{
+    [Cmdlet(VerbsCommon.Copy, "Resx", DefaultParameterSetName = "DefaultParameterSet")]
+    public class GetNotificationChannelCommand: PSCmdlet
+    {
+        private string _path;
+
+        #region Parameters
+
+        [Parameter(Position = 0, ParameterSetName = "DefaultParameterSet", Mandatory = true)]
+        [ValidateNotNullOrEmpty]
+        public string Path
+        {
+            get { return _path; }
+            set { _path = value; }
+        }
+
+
+        #endregion
+
+
+        #region Overrides
+
+        protected override void BeginProcessing()
+        {
+
+        }
+
+        protected override void ProcessRecord()
+        {
+
+        }
+
+        #endregion
+    }
+}
