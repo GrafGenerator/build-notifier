fbed1deabab6d18c8b32d83d50301496967a4292
nikita ivanov <ni.ivanov@2gis.ru>
2016-04-08 18:32:11 +0600

    sample script

---
+++ sample/simple.ps1
@@ -0,0 +1,25 @@
+Set-StrictMode -Version Latest
+Clear
+#=============================
+
+
+$channelName = 'reporting-builds'
+
+$channel = Open-NotificationChannel $channelName
+$conf = New-BuildConfiguration
+
+$messages = @(
+    New-BuildMessage $conf, "Init", "Message 1"
+    New-BuildMessage $conf, "Start", "Message 2"
+    New-BuildMessage $conf, "Progress", "Message 3"
+    New-BuildMessage $conf, "Warning", "Message 4"
+    New-BuildMessage $conf, "Error", "Message 5"
+    New-BuildMessage $conf, "Finish", "Message 6"
+)
+
+foreach($message in $messages){
+    Write-Host "Sending message '$($message.Message)'"
+    Push-Notification $channel $message
+    
+    Start-Sleep -s 2
+}
