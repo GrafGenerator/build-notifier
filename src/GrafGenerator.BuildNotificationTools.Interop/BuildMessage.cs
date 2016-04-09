0600653394e323c948515f96922a792379269ea8
Nikita Ivanov <grafgenerator@gmail.com>
2016-04-09 23:59:04 +0600

    try manual build message serialization

--- src/GrafGenerator.BuildNotificationTools.Interop/BuildMessage.cs
+++ src/GrafGenerator.BuildNotificationTools.Interop/BuildMessage.cs
@@ -1,30 +1,49 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace GrafGenerator.BuildNotificationTools.Interop
 {
     [Serializable]
-    public class BuildMessage
+    public class BuildMessage: ISerializable
     {
-        public Guid BuildId { get; set; }
-        public BuildMessageKind MessageKind { get; set; }
-        public string Message { get; set; }
+	    private readonly Guid _buildId;
+	    private readonly BuildMessageKind _messageKind;
+	    private readonly string _message;
 
-        public BuildMessage()
-        {
-            
-        }
+	    public Guid BuildId => _buildId;
+	    public BuildMessageKind MessageKind => _messageKind;
+	    public string Message => _message;
+
+	    public BuildMessage()
+	    {
+		    
+	    }
 
         private BuildMessage(Guid buildId, BuildMessageKind messageKind, string message)
         {
-            BuildId = buildId;
-            MessageKind = messageKind;
-            Message = message;
+            _buildId = buildId;
+            _messageKind = messageKind;
+            _message = message;
         }
 
+	    protected BuildMessage(SerializationInfo info, StreamingContext context)
+	    {
+		    _buildId = (Guid) info.GetValue("id", typeof (Guid));
+		    _messageKind = (BuildMessageKind) info.GetValue("kind", typeof (BuildMessageKind));
+		    _message = info.GetString("message");
+	    }
 
-        public static BuildMessage Create(Guid buildId, BuildMessageKind messageKind, string message)
+
+		public static BuildMessage Create(Guid buildId, BuildMessageKind messageKind, string message)
         {
             return new BuildMessage(buildId, messageKind, message);
         }
+
+	    public virtual void GetObjectData(SerializationInfo info, StreamingContext context)
+	    {
+		    info.AddValue("id", _buildId);
+			info.AddValue("kind", _messageKind);
+			info.AddValue("message", _message);
+	    }
     }
 }
