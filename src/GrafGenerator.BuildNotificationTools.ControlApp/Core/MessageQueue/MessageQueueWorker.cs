b6f0a8bab0972157cde3da92e57bbf62a4092ba9
Nikita Ivanov <grafgenerator@gmail.com>
2016-04-11 23:59:26 +0600

    add stub core classes

---
+++ src/GrafGenerator.BuildNotificationTools.ControlApp/Core/MessageQueue/MessageQueueWorker.cs
@@ -0,0 +1,19 @@
+﻿using GrafGenerator.BuildNotificationTools.Interop;
+
+namespace GrafGenerator.BuildNotificationTools.ControlApp.Core.MessageQueue
+{
+	class MessageQueueWorker
+	{
+		private readonly NotificationChannel _channel;
+
+		public MessageQueueWorker(NotificationChannel channel)
+		{
+			_channel = channel;
+		}
+
+		public void Watch()
+		{
+			
+		}
+	}
+}
