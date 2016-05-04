0046d3287140cc0c54851abb1d967c50d690c728
Nikita Ivanov <grafgenerator@gmail.com>
2016-05-05 00:01:11 +0600

    notiy on build info received

---
+++ src/GrafGenerator.BuildNotificationTools.ControlApp/Model/BuildInfoReceivedArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GrafGenerator.BuildNotificationTools.ControlApp.Model
+{
+	public class BuildInfoReceivedArgs: EventArgs
+	{
+		public BuildInfo[] BuildInfos { get; }
+
+		private BuildInfoReceivedArgs(BuildInfo[] buildInfos)
+		{
+			BuildInfos = buildInfos;
+		}
+
+
+		public static BuildInfoReceivedArgs Create(BuildInfo[] buildInfos)
+		{
+			return new BuildInfoReceivedArgs(buildInfos);
+		}
+	}
+}
--- src/GrafGenerator.BuildNotificationTools.ControlApp/Model/BuildMessagesStorageService.cs
+++ src/GrafGenerator.BuildNotificationTools.ControlApp/Model/BuildMessagesStorageService.cs
@@ -1,33 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using GrafGenerator.BuildNotificationTools.Interop;
+using System.Collections.Specialized;
 using GrafGenerator.FunctionalExtensions;
 
 namespace GrafGenerator.BuildNotificationTools.ControlApp.Model
 {
 	public class BuildMessagesStorageService : IBuildMessagesStorageService
 	{
-        public BuildInfoCollection BuildMessages { get; private set; }
+        public BuildInfoCollection BuildMessages { get; }
+
+		public event EventHandler<BuildInfoReceivedArgs> BuildInfoReceived; 
 
         public BuildMessagesStorageService()
         {
-            var id = Guid.NewGuid();
-
             BuildMessages = new BuildInfoCollection();
 
-            BuildMessages.Add(BuildInfo.Create(id, BuildMessageKind.Start, "Message 1", DateTime.Now.Ticks));
-            BuildMessages.Add(BuildInfo.Create(id, BuildMessageKind.Init, "Message 2", DateTime.Now.Ticks));
-            BuildMessages.Add(BuildInfo.Create(id, BuildMessageKind.Progress, "Message 3", DateTime.Now.Ticks));
-            BuildMessages.Add(BuildInfo.Create(id, BuildMessageKind.Unknown, "Message 4", DateTime.Now.Ticks));
-            BuildMessages.Add(BuildInfo.Create(id, BuildMessageKind.Warning, "Message 5", DateTime.Now.Ticks));
-            BuildMessages.Add(BuildInfo.Create(id, BuildMessageKind.Error, "Message 6", DateTime.Now.Ticks));
-            BuildMessages.Add(BuildInfo.Create(id, BuildMessageKind.Finish, "Message 7", DateTime.Now.Ticks));
+			BuildMessages.CollectionChanged += BuildMessagesOnCollectionChanged;
         }
 
-        public Result AddMessage(BuildInfo message)
+		public Result AddMessage(BuildInfo message)
         {
             BuildMessages.Add(message);
             return Result.Ok();
         }
-    }
+
+
+		private void BuildMessagesOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs eventArgs)
+		{
+			if (eventArgs.Action == NotifyCollectionChangedAction.Add)
+			{
+				var buildInfos = new BuildInfo[eventArgs.NewItems.Count];
+				eventArgs.NewItems.CopyTo(buildInfos, 0);
+
+				OnBuildInfoReceived(BuildInfoReceivedArgs.Create(buildInfos));
+			}
+		}
+
+
+		protected virtual void OnBuildInfoReceived(BuildInfoReceivedArgs e)
+		{
+			BuildInfoReceived?.Invoke(this, e);
+		}
+	}
 }
